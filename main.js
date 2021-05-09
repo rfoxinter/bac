746c2bce64f06889d4a4479d80d12c9ac4f9ec75
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-05-09 20:20:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ function pdf(a) {
 	var author = document.getElementById("author").textContent;
 	a.setAttribute("href","../pdfs/"+title+" - "+author+".pdf")
 }
+
+
+function character_map(a) {
+	var title = document.getElementById("title").textContent;
+	var author = document.getElementById("author").textContent;
+	a.setAttribute("href","../character_maps/"+title+" - "+author+".pdf")
+}
