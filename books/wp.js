ddb6e004243b818f1f53a48d9cea1bb0002bdde1
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-05-23 14:51:05 +0200

    Delete wp.js

--- books/wp.js
+++
@@ -1,9 +0,0 @@
-function poems() {
-    var x = document.getElementById("poems");
-    if (document.getElementById("poems").style.display === "none") {
-        document.getElementById("poems").style.display = "block";
-    }
-    else {
-        document.getElementById("poems").style.display = "none";
-    }
-}
