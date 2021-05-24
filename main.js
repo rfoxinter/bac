032ee371241f0536d08f72dc3ad2dd1aaae81382
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-05-24 11:10:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,30 +3,39 @@
 function online(a) {
 	var title = document.getElementById("title").textContent;
 	var author = document.getElementById("author").textContent;
-	a.setAttribute("href","https://rfoxinter.github.io/ebooks/bibi/?book="+title+" - "+author+".epub")
+	a.setAttribute("href","https://rfoxinter.github.io/ebooks/bibi/?book="+title+" - "+author+".epub");
 }
 
 function pdf(a) {
 	var title = document.getElementById("title").textContent;
 	var author = document.getElementById("author").textContent;
-	a.setAttribute("href","../pdfs/"+title+" - "+author+".pdf")
+	a.setAttribute("href","../pdfs/"+title+" - "+author+".pdf");
 }
 
 function ebook(a,format) {
 	var title = document.getElementById("title").textContent;
 	var author = document.getElementById("author").textContent;
-	a.setAttribute("href","../ebooks/"+title+" - "+author+"."+format)
+	a.setAttribute("href","../ebooks/"+title+" - "+author+"."+format);
 }
 
 function character_map(a) {
 	var title = document.getElementById("title").textContent;
 	var author = document.getElementById("author").textContent;
-	a.setAttribute("href","../character_maps/"+title+" - "+author+".pdf")
+	a.setAttribute("href","../character_maps/"+title+" - "+author+".pdf");
 }
 
-function quotes(a,book) {
-	var quizlet = {tiobe : "https://quizlet.com/_9lfo98?x=1jqt&i=3hrg84"}
-	a.setAttribute("href",quizlet[book])
+function quotes(a) {
+	function book_id(str) {
+		str = str.substring(0,str.length-5);
+		while (str.indexOf("/") != -1) {
+			str = str.substring(str.indexOf("/")+1,str.length);
+		}
+		return str
+	}
+	var quizlet = {tiobe : "https://quizlet.com/_9lfo98?x=1jqt&i=3hrg84"};
+	var url = window.location.href;
+	url = book_id(url)
+	a.setAttribute("href",quizlet[url]);
 }
 
 function ebooks() {
