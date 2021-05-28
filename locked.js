e95c6d66f5e1df5a45fbdd6424a9e5bea78c8673
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-05-28 15:39:02 +0200

    Create locked.js

---
+++ locked.js
@@ -0,0 +1,14 @@
+function checkPass() {
+    let params = new URLSearchParams(document.location.search.substring(1));
+    const id = params.get("id");
+    const title = params.get("title");
+    const author = params.get("author");
+    const pwd = id + "2021";
+
+    if (document.f_pass.pass.value == pwd) {
+        window.open("./character_maps/" + title + " - " + author + ".pdf", target="_self");
+    }
+    else {
+        document.getElementById("alert").innerHTML = "The password is not correct";
+    }
+}
