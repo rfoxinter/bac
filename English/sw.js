cca4a2f96482d785594b5f3153e01b62f1bd32f7
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-12-13 19:46:38 +0100

    Update sw.js

--- English/sw.js
+++ English/sw.js
@@ -2,8 +2,8 @@ var APP_PREFIX = 'OIB Books'     // Identifier for this app (this needs to be co
 var VERSION = '1'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/English/',                     // If you have separate JS/CSS files,
-  '/English/index.html'            // add path to those files here
+  '/bac/English/',                     // If you have separate JS/CSS files,
+  '/bac/English/index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
