c83db0411933f002b09c19d75ceb0de78ec17aaf
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2022-01-24 19:51:15 +0100

    Create main.js

---
+++ Mathematiques/main.js
@@ -0,0 +1,29 @@
+function niveau() {
+    const n = document.getElementsByTagName("li");
+    var i=0;
+    while (i < n.length) {
+        var p = document.createElement("p");
+        p.setAttribute("style","white-space: pre;");
+        var t = "";
+        for (var j = -Math.floor((n[i].className.search("<")+(n[i].className.length-n[i].className.search("<")-2)*0.5)/4) + 2; j >= 0; j--) {
+            t += "&Tab;"
+        }
+        p.innerHTML = (n[i].className.substring(0,n[i].className.search("<"))+n[i].className.substring(n[i].className.search("<")+1,n[i].className.search(">")).sup()+t);
+        delete m,t;
+        var tex = document.createElement("img");
+        tex.setAttribute("src","latex.svg");
+        tex.setAttribute("class","latex");
+        var a_tex = document.createElement("a");
+        a_tex.setAttribute("href","Mathematiques"+(n[i].className).replace("<","").replace(">","").replace("è","e")+".tex");
+        a_tex.appendChild(tex);
+        var pdf = document.createTextNode("pdf");
+        var a_pdf = document.createElement("a");
+        a_pdf.setAttribute("href","Mathematiques"+(n[i].className).replace("<","").replace(">","").replace("è","e")+".pdf");
+        a_pdf.appendChild(pdf);
+        p.appendChild(a_tex);
+        p.innerHTML += "&Tab;&Tab;"
+        p.appendChild(a_pdf);
+        n[i].appendChild(p);
+        i=i+1;
+    }
+}
