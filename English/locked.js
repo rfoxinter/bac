ef859cba38363287fc709810e221b6403808df0f
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2022-02-09 11:30:43 +0100

    Update locked.js

--- English/locked.js
+++ English/locked.js
@@ -1,10 +1,10 @@
 function checkPass() {
     let params = new URLSearchParams(document.location.search.substring(1));
-    const id = params.get("id");
+    const id = btoa(params.get("id"));
     const title = params.get("title");
     const author = params.get("author");
     const type = params.get("type");
-    const pwd = btoa(id.toUpperCase() + "2022");
+    const pwd = btoa(atob(id).toUpperCase() + "2022");
 
     if (btoa(document.f_pass.pass.value) == pwd) {
         window.open("./" + type + "/" + title + " - " + author + ".pdf", target="_self");
